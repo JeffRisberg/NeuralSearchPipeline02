72a16f0357c1bb5ca42f8bfe24fd4d87997eebb7
JeffRisberg <jrisberg@aol.com>
2022-06-24 20:02:38 -0700

    fixed pipeline problem

--- src/load_and_run_pipeline.py
+++ src/load_and_run_pipeline.py
@@ -8,14 +8,25 @@ def clean_text(text):
   return ''.join(x for x in text.lower() if x in string.printable).replace(
       '.', '')
 
-
-pipeline = Pipeline()
-
 validate_yaml(Path("sample.haystack-pipeline.yml"))
 
-pipeline.load_from_yaml(Path("sample.haystack-pipeline.yml"), pipeline_name='my_query_pipeline')
-print(pipeline)
-print(pipeline.root_node)
+def show_results(results):
+  print(len(results['documents']), "documents")
+  for document in results['documents']:
+    print(document.meta)
+    print(document.score)
 
-result = pipeline.run(query=clean_text("Convertible Notes and SAFE Terms and Definitions"))
-print(result)
+def run_query(query):
+  print("query:", query)
+  results = pipeline.run(query=clean_text(query))
+  show_results(results)
+  print()
+
+pipeline = Pipeline.load_from_yaml(Path("sample.haystack-pipeline.yml"), pipeline_name='my_query_pipeline')
+
+query = "Convertible Notes and SAFE Terms and Definitions"
+run_query(query)
+
+query = "Can I contact someone to discuss my 409A valuation?"
+run_query(query)
+
